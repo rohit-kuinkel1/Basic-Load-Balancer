477778da97469c5a42d6e9022dfbc15906df8fb3
RK69 <rohit.kuinkel@protonmail.com>
2024-12-09 19:20:42 +0100

    add IScaler and SystemMetrics

---
+++ AutoScaler/IScaler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using LoadBalancer;
+
+namespace AutoScaler
+{
+    public interface IScaler
+    {
+        Task SpawnServerAsync();
+        Task RemoveServerAsync( IServer server );
+        Task MonitorAndScaleAsync();
+        Task<SystemMetrics> GetCurrentMetricsAsync();
+        bool CanScaleUp();
+        bool CanScaleDown();
+    }
+}
---
+++ AutoScaler/SystemMetrics.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoScaler
+{
+    public class SystemMetrics
+    {
+        public double AverageResponseTime { get; set; }
+        public int TotalActiveConnections { get; set; }
+        public int ServerCount { get; set; }
+        public double CpuUtilization { get; set; }
+        public double MemoryUtilization { get; set; }
+    }
+}
