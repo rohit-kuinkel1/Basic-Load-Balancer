1007ef6af1d59661cf323900dbfc0c4fff112281
RK69 <rohit.kuinkel@protonmail.com>
2024-12-10 19:50:31 +0100

    add exceptions

---
+++ SharedAbstractions/SharedExceptions/Exceptions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LoadBalancer.Exceptions
+{
+    /// <summary>
+    /// Base exception class for all load balancer related exceptions
+    /// </summary>
+    public class LoadBalancerException : Exception
+    {
+        public string ErrorCode { get; }
+        public DateTime Timestamp { get; }
+
+        public LoadBalancerException(string message)
+            : base(message)
+        {
+            Timestamp = DateTime.UtcNow;
+            ErrorCode = "LB-GEN-001";
+        }
+
+        public LoadBalancerException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+            Timestamp = DateTime.UtcNow;
+            ErrorCode = "LB-GEN-001";
+        }
+
+        public LoadBalancerException(string message, string errorCode)
+            : base(message)
+        {
+            Timestamp = DateTime.UtcNow;
+            ErrorCode = errorCode;
+        }
+
+        public LoadBalancerException(string message, string errorCode, Exception innerException)
+            : base(message, innerException)
+        {
+            Timestamp = DateTime.UtcNow;
+            ErrorCode = errorCode;
+        }
+    }
+}
---
+++ SharedAbstractions/SharedExceptions/HealthCheckException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LoadBalancer.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when health checks fail for nodes in the load balancer
+    /// </summary>
+    public class HealthCheckException : LoadBalancerException
+    {
+        public string NodeId { get; }
+        public DateTime LastSuccessfulCheck { get; }
+        public int FailedAttempts { get; }
+
+        public HealthCheckException(string nodeId, DateTime lastSuccessfulCheck, int failedAttempts, string message)
+            : base(message, "LB-HEALTH-001")
+        {
+            NodeId = nodeId;
+            LastSuccessfulCheck = lastSuccessfulCheck;
+            FailedAttempts = failedAttempts;
+        }
+
+        public HealthCheckException(string nodeId, DateTime lastSuccessfulCheck, int failedAttempts, string message, Exception innerException)
+            : base(message, "LB-HEALTH-001", innerException)
+        {
+            NodeId = nodeId;
+            LastSuccessfulCheck = lastSuccessfulCheck;
+            FailedAttempts = failedAttempts;
+        }
+    }
+}
