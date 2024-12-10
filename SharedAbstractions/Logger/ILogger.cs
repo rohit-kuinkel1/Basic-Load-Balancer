c6e7f5dc893cc958f3b566f38f9bec9e915abb6f
RK69 <rohit.kuinkel@protonmail.com>
2024-12-10 20:23:40 +0100

    add logger

---
+++ SharedAbstractions/Logger/ILogger.cs
@@ -0,0 +1,9 @@
+﻿namespace LoadBalancer.Logging
+{
+    public interface ILogger : IDisposable
+    {
+        void Log(LogLevel level, string message, Exception? exception = null);
+        bool ShouldLog(LogLevel level);
+        void Flush();
+    }
+}
---
+++ SharedAbstractions/Logger/LogEnums.cs
@@ -0,0 +1,19 @@
+﻿namespace LoadBalancer.Logging
+{
+    public enum LogLevel
+    {
+        Trace = 0,
+        Debug = 1,
+        Info = 2,
+        Warn = 3,
+        Error = 4,
+        Fatal = 5
+    }
+
+    public enum LogDestination
+    {
+        Console = 0,
+        File = 1,
+        ConsoleAndFile = 2,
+    }
+}
