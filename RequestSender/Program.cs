a66e00682703b9c3d0c319f4a49bf44a70e60378
RK69 <rohit.kuinkel@protonmail.com>
2024-12-14 17:47:22 +0100

    add RequestSender to mimick request sending to loadbalancer

---
+++ RequestSender/Program.cs
@@ -0,0 +1,85 @@
+﻿using LoadBalancer.Exceptions;
+using LoadBalancer.Logger;
+
+namespace LoadBalancer
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                Log.SetMinimumLevel(LogLevel.Trace);
+                Log.AddSink(
+                    LogSinks.ConsoleAndFile,
+                    Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                        "LoadBalancerLogs"
+                    )
+                );
+
+                //we will make sure of singelton later on with builder services
+                var loadBalancer = new LoadBalancer(
+                                        loadBalancingStrategy: new RoundRobinStrategy(),
+                                        httpClient: new HttpClient(),
+                                        enabledAutoScaling: true,
+                                        autoScalingConfig: AutoScalingConfig.Factory(),
+                                        healthCheckInterval: TimeSpan.FromSeconds(10),
+                                        minHealthThreshold: 90
+                );
+
+
+                List<(int DurationInSeconds, int RequestsPerSecond)> TrafficPatterns = new()
+                {
+                    (5, 1),
+                    (5, 1000),
+                    (9, 40000),
+                    (5, 20000),
+                };
+
+                foreach (var pattern in TrafficPatterns)
+                {
+                    await SimulateTraffic(loadBalancer, pattern.RequestsPerSecond, pattern.DurationInSeconds);
+                }
+            }
+            catch (Exception ex) when (ex is LoadBalancerException)
+            {
+                Log.Error("An error occurred", ex);
+                Environment.Exit(1);
+            }
+        }
+
+        private static async Task SimulateTraffic(LoadBalancer loadBalancer, int requestsPerSecond, int durationInSeconds)
+        {
+            Log.Info($"Simulating traffic: {requestsPerSecond} requests/second for {durationInSeconds} seconds.");
+
+            var tasks = new List<Task>();
+            var endTime = DateTime.UtcNow.AddSeconds(durationInSeconds);
+
+            while (DateTime.UtcNow < endTime)
+            {
+                for (int i = 0; i < requestsPerSecond; i++)
+                {
+                    tasks.Add(Task.Run(async () =>
+                    {
+                        var dummyRequest = new HttpRequestMessage(HttpMethod.Get, "http://localhost");
+                        var wasRequestHandled = await loadBalancer.HandleRequestAsync(dummyRequest);
+                        if (wasRequestHandled)
+                        {
+                            Log.Info("Request: OK");
+                        }
+                        else
+                        {
+                            Log.Fatal("Request: Failed");
+                        }
+                    }));
+                }
+
+                await Task.Delay(2000);
+            }
+
+            await Task.WhenAll(tasks);
+            Log.Info($"Finished traffic simulation: {requestsPerSecond} requests/second.");
+        }
+    }
+}
